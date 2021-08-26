f0729ecc229961c6e15d6aae5349c1172b7bbb2a
xiebian <xiebianwhu@gmail.com>
2021-08-26 14:54:03 +0800

    add store product display

---
+++ src/components/store/ProductDisplay.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        sm="6"
+        md="4"
+        v-for="product in $store.state.products"
+        :key="product.name"
+      >
+      <VerticalCard :product="product" :addToCart="addToCart"/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import VerticalCard from "../cards/VerticalCard.vue";
+
+export default {
+  components: {
+    VerticalCard,
+  },
+
+  methods: {
+    addToCart(index, quantity = 1) {
+      this.$store.commit("addItemToCart", { itemId: index, quantity });
+      this.$store.commit("updateSnackbar", { show: true });
+    },
+  },
+};
+</script>
--- src/views/Store.vue
+++ src/views/Store.vue
@@ -5,13 +5,17 @@
                 Sidebar
             </v-col>
             <v-col sm="9" lg="7">
-                Products
+                <ProductDisplay />
             </v-col>
         </v-row>
     </v-container>
 </template>
 <script>
+import ProductDisplay from '../components/store/ProductDisplay.vue';
+
 export default {
-    
+    components: {
+        ProductDisplay,
+    }   
 }
 </script>
