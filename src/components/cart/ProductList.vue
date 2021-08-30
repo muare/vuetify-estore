17f746bea2a48cba3987de103a3dc8a970c22887
xiebian <xiebianwhu@gmail.com>
2021-08-30 13:16:17 +0800

    fix cart page layout to align product list and checkout box by wrap product list via v-container

--- src/components/cart/ProductList.vue
+++ src/components/cart/ProductList.vue
@@ -1,19 +1,17 @@
 <template>
-    <div>
-        <v-row
-        v-for="product in $store.state.products"
-        :key="product.name">
-        <HorizontalProduct :product="product"/>
-        </v-row>
-    </div>
+  <v-container>
+    <v-row v-for="product in $store.state.products" :key="product.name">
+      <HorizontalProduct :product="product" />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import HorizontalProduct from '../cards/HorizontalCard.vue';
+import HorizontalProduct from "../cards/HorizontalCard.vue";
 
 export default {
-    components: {
-        HorizontalProduct,
-    }
-}
+  components: {
+    HorizontalProduct,
+  },
+};
 </script>
